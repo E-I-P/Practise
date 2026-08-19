61a5b71b33e2d10df3b6d0b79519132394e8f01b
Takunda Chigwedere <takuehope@gmail.com>
2026-08-19 10:27:53 +0200

    Add typecasting example with age and gpa variables

---
+++ Python/typecasting.py
@@ -0,0 +1,10 @@
+#Typecasting = process of converting a variablefrom one data type to another data type.
+
+age = 123
+gpa = 4.4
+
+gpa = int(gpa) #Typecasting float to int
+print(gpa)
+
+age = float(age) #Typecasting int to float
+print(age)
